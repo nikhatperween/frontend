baafa9f1f516ff064a237b6e8cfb429a336214e9
nikhatperween <nperween2714@gmail.com>
2024-06-26 22:59:49 +0530

    add changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { TestService } from './test.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'my-app';
+
+  constructor(private service: TestService){}
+  ngOnInit(): void {
+    this.getdata()
+  }
+
+  data: any
+  getdata(){
+    this.service.getData().subscribe((resp)=>{
+      console.log(resp)
+      this.data = resp
+    }
+  )
+  }
+
 }
